08709282659d5909987dc4be608f73f73210666a
“RTNex” <rtnexcorp@gmail.com>
2026-01-11 15:16:28 +0530

    graphql running

---
+++ graphql/src/main/java/org/apache/sling/cms/graphql/internal/fetchers/HelloDataFetcher.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.cms.graphql.internal.fetchers;
+
+import org.apache.sling.graphql.api.SlingDataFetcher;
+import org.apache.sling.graphql.api.SlingDataFetcherEnvironment;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Data fetcher for the hello GraphQL query.
+ * Returns a welcome message for the Sling CMS GraphQL API.
+ */
+@Component(
+        service = SlingDataFetcher.class,
+        property = {"name=slingcms/hello"})
+public class HelloDataFetcher implements SlingDataFetcher<String> {
+
+    @Override
+    public String get(SlingDataFetcherEnvironment environment) throws Exception {
+        return "Apache Sling CMS GraphQL API - Version 1.1.9 | Ready to serve content queries | "
+                + "Use this endpoint to access CMS content, pages, assets, and metadata through GraphQL queries.";
+    }
+}
---
+++ graphql/src/main/java/org/apache/sling/cms/graphql/internal/schema/SlingCmsSchemaProvider.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.cms.graphql.internal.schema;
+
+import java.io.IOException;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.graphql.api.SchemaProvider;
+import org.jetbrains.annotations.NotNull;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Custom schema provider for Sling CMS GraphQL that returns the schema
+ * as a string to avoid recursive execution of .gql files.
+ */
+@Component(
+        service = SchemaProvider.class,
+        property = {"name=slingcms/schema"})
+public class SlingCmsSchemaProvider implements SchemaProvider {
+
+    private static final Logger log = LoggerFactory.getLogger(SlingCmsSchemaProvider.class);
+
+    private static final String SCHEMA =
+            "type Query {\n" + "  hello: String @fetcher(name: \"slingcms/hello\")\n" + "}\n";
+
+    @Override
+    public String getSchema(@NotNull Resource schemaResource, String[] selectors) throws IOException {
+        log.debug("Providing schema for resource: {} with selectors: {}", schemaResource.getPath(), selectors);
+        return SCHEMA;
+    }
+}
